1cd631ed5f8a4f02c80236dab53a6b252e57cb51
Markiian Yuskevych <markiyan.yuskevych@gmail.com>
2017-10-09 16:31:01 +0300

    added customkey and partitioner

---
+++ src/main/java/utilClasses/CustomKey.java
@@ -0,0 +1,91 @@
+package utilClasses;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class CustomKey implements WritableComparable<CustomKey> {
+    private Integer cityID;
+    private Integer dataSetType;
+
+    public CustomKey() {
+    }
+
+    public CustomKey(Integer userId, Integer dataSetType) {
+        super();
+        this.cityID = userId;
+        this.dataSetType = dataSetType;
+    }
+
+    public Integer getUserId() {
+        return cityID;
+    }
+
+    public Integer getDataSetType() {
+        return dataSetType;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(cityID);
+        out.writeInt(dataSetType);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        cityID = in.readInt();
+        dataSetType = in.readInt();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((dataSetType == null) ? 0 : dataSetType.hashCode());
+        result = prime * result + ((cityID == null) ? 0 : cityID.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CustomKey other = (CustomKey) obj;
+        if (dataSetType == null) {
+            if (other.dataSetType != null)
+                return false;
+        } else if (!dataSetType.equals(other.dataSetType))
+            return false;
+        if (cityID == null) {
+            if (other.cityID != null)
+                return false;
+        } else if (!cityID.equals(other.cityID))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(CustomKey o) {
+        int returnValue = compare(cityID, o.getUserId());
+        if (returnValue != 0) {
+            return returnValue;
+        }
+        return compare(dataSetType, o.getDataSetType());
+    }
+
+    public static int compare(int k1, int k2) {
+        return (k1 < k2 ? -1 : (k1 == k2 ? 0 : 1));
+    }
+
+    @Override
+    public String toString() {
+        return "UtilityClasses.CustomKey [userId=" + cityID + ", dataSetType=" + dataSetType + "]";
+    }
+
+}
---
+++ src/main/java/utilClasses/DataParser.java
@@ -0,0 +1,44 @@
+package utilClasses;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataParser {
+
+    private String line;
+    private List<String> words;
+
+    private final int CITY_ID = 7;
+    private final int REGION_ID = 6;
+    private final int MAX_CITY_INDEX = 395;
+
+    private final int MIN_BIDDING_PRICE = 250;
+    private final int BIDDING_PRICE_INDEX = 19;
+
+    private final int NORMAL_SIZE = 16;
+
+    public DataParser(String line) {
+        this.line = line;
+        words = Arrays.asList(line.split("\t"));
+    }
+
+    public int getCityID() {
+        return Integer.parseInt(words.get(CITY_ID));
+    }
+
+    public int getRegionID() {
+        return Integer.parseInt(words.get(REGION_ID));
+    }
+
+    public boolean isValidForMapping() {
+        return words.size() == 26 && getCityID() <= MAX_CITY_INDEX && getBiddingPrice() > MIN_BIDDING_PRICE;
+    }
+
+    public int getBiddingPrice() {
+        return Integer.parseInt(words.get(BIDDING_PRICE_INDEX));
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
+}
